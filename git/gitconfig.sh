78e5bda53efe10870ffc8b2b4cb60c17b9b7cb69
mafulong <mafulong@bytedance.com>
2019-08-05 14:51:26 +0800

    fix

--- git/gitconfig.sh
+++ git/gitconfig.sh
@@ -1,5 +1,7 @@
-remote_setting=""
+remote_setting="https://raw.githubusercontent.com/mafulong/mafulong-scirpt/master/git/gitconfig"
 cd ~
-curl -s ${remore_setting} -a mafulogn_remote_setting
-cat mafulogn_remote_setting > .gitconfig
-rm mafulogn_remote_setting
+cp .gitconfig .gitconfig_backup
+echo ${remote_setting}
+curl -s ${remote_setting} -o mafulong_remote_setting
+cat mafulong_remote_setting >> .gitconfig
+rm mafulong_remote_setting
